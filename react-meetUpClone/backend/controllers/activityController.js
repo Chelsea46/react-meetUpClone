ca42920eefe53feda3f1bc4d5e6c65ad5b60fa38
chelsea roberts <chelsea.j.roberts@hotmail.com>
2023-06-15 13:31:46 +0100

    asyncHandler install

--- react-meetUpClone/backend/controllers/activityController.js
+++ react-meetUpClone/backend/controllers/activityController.js
@@ -1,27 +1,28 @@
-
+import {asyncHandler} from 'express-async-handler'
 // Get Activity, GET /api/activity
-const getActivity = (req, res) => {  
+const getActivity = asyncHandler(async (req, res) => {  
     res.status(200).json({ message : 'Get activity' })
-}
+})
 
 // Create Activity, POST /api/activity
-const createActivity = (req, res) => {
+const createActivity = asyncHandler(async (req, res) => {
     if(!req.body.text){
         res.status(400)
         throw new Error('Please add text field')
     }
     res.status(200).json({ message : 'Create (set) activity' })
-}
+})
 
 // Update Activity, PUT /api/activity/:id
-const updateActivity = (req, res) => {
+const updateActivity = asyncHandler(async (req, res) => {
     res.status(200).json({ message : `Update activity ${req.params.id} `})
-}
+})
 
 //  Delete Activity, DELETE /api/activity/:id
-const removeActivity = (req, res) => {
+const removeActivity = asyncHandler(async (req, res) => {
     res.status(200).json({ message : `Delete activity ${req.params.id} `})
-}
+})
+
 export {
     getActivity,
     createActivity,
